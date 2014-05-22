2062b8c87e8e4d98a05269637cd5872ffa2f142b
Modjor <modjor@badabing.fr>
2014-05-22 19:13:11 +0200

    Add IMDB Watch list support and debug mode

--- SickAdd.py
+++ SickAdd.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 #
 ##################################################################################
-### SickAdd V. 1.0  - THIS IS AN ALPHA RELEASE
+### SickAdd V. 2  - THIS IS AN ALPHA RELEASE
 #
 # This script downloads your TVDB favorites and add them to your SickBeard shows
 #
 # NOTE: This script requires Python to be installed on your system
+#
+#
+# Changelog
+# Version 2
+# - Add IMDB Watch list support (using IMDB Mapping from TVDB)
+# - Add a Debug mode so it's a bit less verbose in standard mode
+#
 ##################################################################################
 
 
@@ -28,10 +35,20 @@ webroot = ""
 
 #TVDB Configuration
 tvdb_accountid= "your_tvdb_account_id"
-tvdb_enabled = 1
+tvdb_enabled = 0
+
+# IMDB Configuration
+imdb_watchlist_rss_url = "path_to_imdb_watchlist_rss"
+imdb_enabled = 0
 
 
 
+# Advanced Settings
+debug = 0
+
+
+
+#########    NO MODIFICATION UNDER THAT LINE
 ##########################################################
 
 
@@ -43,11 +60,13 @@ import os.path
 import json
 from lxml import etree
 from StringIO import StringIO
+import shutil
 
-tvdb_api_url = "http://thetvdb.com/api"
+
+db_version_must_be = 2
 sickadd_db = install_path + "/" + db_name
-sickadd_version = 1
 sickbeard_api_url = "http://" + sickbeard_host + ":" + sickbeard_port + "/api/" + sickbeard_api
+
 	
 ## Database creation
 def db_creation():
@@ -64,7 +83,9 @@ def db_creation():
 
 
 
-## DB Check - checks if db exists
+##############################   DATABASE OPERATIONS  ############################
+
+## DB Check - checks if db exists or if needs to be upgraded
 def db_check():
 	print "Initializing database " + sickadd_db
 	if not os.path.isfile(sickadd_db):
@@ -72,27 +93,82 @@ def db_check():
 		db_creation()
 		
 	else:
-		print db_name + " found. Continuing..."
-
-
-
-
-## Is TVDB processing is enabled?
-def tvdb_check():
-	if tvdb_enabled == 1:
-		tvdb_processing()
-	else: 
-		print ("TVDB processing is disabled")
-		print ("Please configure and enable TVDB in your settings")
-		sys.exit()
-
+		if debug == 1:
+			print db_name + " found. Checking Database version"
+		conn = sqlite3.connect(sickadd_db)
+		c = conn.cursor()
+		c.execute ("SELECT db_version FROM info WHERE db_version NOT NULL")
+		result_set = c.fetchall()
+		value =  result_set[0]
+		current_db_version = str(value[0])
+		
+		if debug == 1:
+			print "Current Database version is: " + current_db_version
+			print "Database version must be: " + str(db_version_must_be)
+			
+		if int(current_db_version) > db_version_must_be:
+			print "Your database file version greater than what this version of SickAdd can support"
+			print "Please upgrade SickAdd or delete your DB file"
+			print "Exiting..."
+			conn.close()
+			sys.exit()
+			
+		if int(current_db_version) == db_version_must_be:
+			if debug == 1:
+				print "Your database is using current schema. No need for update"
+			conn.close()
+		else:
+			print "Your database scheme has to be upgraded"
+			conn.close()
+			db_upgrade(current_db_version)
+	
 	
 
-	
-## TVDB processing
+
+
+## DB Upgrade. Used if current DB file has to be upgraded to match current SickAdd version	
+def db_upgrade(current_db_version):	
+	conn = sqlite3.connect(sickadd_db)
+	c = conn.cursor()	
+	if 	int(current_db_version) == 1:
+#	UPGRADE TO VERSION 2 ANY DB IN VERSION 1
+		db_bck = (sickadd_db + "_v1")		
+		if debug == 1:
+			print "Backing up database file as: "  + db_bck
+			print "Upgrading database to version 2..."
+		shutil.copyfile(sickadd_db, db_bck)
+		c.execute ("CREATE TABLE imdb_fav (imdb_id text PRIMARY KEY, imdb_name text, tvdb_id INTEGER)")
+		c.execute ("UPDATE info SET db_version = 2 WHERE db_version = 1")
+		conn.commit()
+		c.execute ("SELECT db_version FROM info WHERE db_version NOT NULL")
+		result_set = c.fetchall()
+		value =  result_set[0]
+		current_db_version = value[0]
+		conn.close()
+		if debug == 1:
+			print "DB should be now in version "+ str(current_db_version)
+		if current_db_version == 2:
+			if debug == 1:
+				print "Your database has been upgraded to version 2"
+			db_check()
+		else:
+			print "Database is NOT in version 2"
+			print "Error during database upgrade. Exiting..."
+			sys.exit()
+			
+			
+		
+
+
+
+		
+##############################  TVDB FAVORITES PROCESSING  ############################		
+
+## TVDB processing -  download TVDB favorites and add them to SickAdd db
 def tvdb_processing():
 #	Get the favorites from TVDB.com
-	print ("Retrieving TVDB Favorites for Account ID: " + tvdb_accountid)
+	if debug == 1:
+		print ("Retrieving TVDB Favorites for Account ID: " + tvdb_accountid)
 	tvdb_fav_url =  tvdb_api_url + "/User_Favorites.php?accountid=" + tvdb_accountid
 	response = urllib2.urlopen(tvdb_fav_url)
 	tvdb_xml = response.read()
@@ -107,18 +183,124 @@ def tvdb_processing():
 	c = conn.cursor()
 	tree = etree.parse(StringIO(tvdb_xml))
 	for Favorites in tree.xpath("/Favorites/Series"):
-		print "Adding show TVDB ID: " + Favorites.text
+		if debug == 1:
+			print "Adding show TVDB ID: " + Favorites.text
 		FavoritesInt = int(Favorites.text)
 		c.execute ("INSERT OR IGNORE INTO tvdb_fav VALUES (?,?,?)", (FavoritesInt, "", 0))
 		conn.commit()
 	print "XML Import Done"
 	conn.close()
 
+	
 
 
-## Checking if sickbeard is running
+	
+##############################  IMDB FAVORITES PROCESSING  ############################	
+	
+## IMDB Processing: Download IMDB RSS whatchlist and store the IMDB_ID and IMDB_Title in the imdb_fav table
+# Download IMDB watchlist and add it to local SickAdd DB 
+def imdb_processing(imdb_watchlist_rss_url):
+#	Get the watchlist from imdb.com
+	imdb_http_status = urllib2.urlopen("http://www.imdb.com").getcode()
+	if imdb_http_status != 200:
+		print "IMDB cannot be contacted. Cancelling IMDB processing"
+		return
+	if debug == 1:
+		print ("Retrieving IMDB watchlist from:")
+		print imdb_watchlist_rss_url
+	response = urllib2.urlopen(imdb_watchlist_rss_url)
+	imdb_rss = response.read()
+	response.close()
+#	Parse the IMDB RSS result from the web response and only add the IMDB TV series to the sickadd database
+	if debug == 1:
+		print "Adding new IMDB IDs to SickAdd database"
+	conn = sqlite3.connect(sickadd_db)
+	c = conn.cursor()
+	tree = etree.parse(StringIO(imdb_rss))
+	for item in tree.iter('item'):
+		imdb_title = item[1].text
+		imdb_link = item[2].text
+		imdb_guid = item[3].text
+		imdb_id = (imdb_guid[-10:])[:9]
+		if debug == 1:
+			print "Title: " + imdb_title
+			print "Link: " + imdb_link
+			print "GUID: " + imdb_guid
+			print "IMDB ID from GUID: " + imdb_id
+		if ("TV Series" in imdb_title) or ("Mini-Series" in imdb_title):
+			if debug == 1:
+				print imdb_title, "seems to be a TV show"
+				print "Adding (if does not already exist)", imdb_title, "to IMDB Favorites"
+			c.execute ("INSERT OR IGNORE INTO imdb_fav VALUES (?,?,?)", (imdb_id, imdb_title, None))
+			conn.commit()
+	if debug == 1:
+		print "End of IMDB watch list Import"
+	conn.close()
+	imdbid_to_tvdbid()
+
+
+## Looks at all IMDB_fav records without a valid TVDB ID and tries to find a match on TVDB:
+def imdbid_to_tvdbid():
+	tvdb_http_status = urllib2.urlopen("http://thetvdb.com").getcode()
+	if tvdb_http_status != 200:
+		print "TheTVDB cannot be contacted. Cancelling process 'IMDB ID TO TVDB ID'"
+		return
+	conn = sqlite3.connect(sickadd_db)
+	c = conn.cursor()
+# Select all records from imdb_fav table where tvdb_id is NULL	
+	c.execute ("SELECT imdb_id, imdb_name FROM imdb_fav WHERE tvdb_id is NULL")
+	result_set = c.fetchall()
+	for imdb_id_list in result_set:
+		imdb_id =  str(imdb_id_list[0])
+		imdb_name = imdb_id_list[1]
+		print "TV show", imdb_name, "does not have a valid TVDB ID. Trying to find one"
+		print "Attempting to match IMDB ID", imdb_id, "with TheTVDB"
+ # 		Request TVDP API using IMDB ID of the show		
+		tvdb_imdbid_request_url = (tvdb_api_url + "/GetSeriesByRemoteID.php?imdbid=" + imdb_id)
+		if debug == 1:
+			print ("Retrieving IMDB Info from TVDB using:")
+			print tvdb_imdbid_request_url
+		response = urllib2.urlopen(tvdb_imdbid_request_url)
+		tvdb_show = etree.parse(StringIO(response.read()))
+# 		Checks if the answer from TVDB contains TVDB data or not		
+		tvdb_id_txt = tvdb_show.find('Series/seriesid')
+		if tvdb_id_txt is None: 
+			print "No record found on TVDB for", imdb_id, "-", imdb_name
+		else:
+# 		If data found, add the TVDB id to the IMDB table for the current IMDB record
+			tvdb_id = int((tvdb_id_txt).text)
+			print "TVDB ID value found for IMDB", imdb_id, ":", str(tvdb_id)
+			c.execute ('UPDATE imdb_fav SET tvdb_id = ? WHERE imdb_id = ? AND tvdb_id is NULL', (tvdb_id, imdb_id))
+			conn.commit()
+
+	conn.close()
+	imdb_table_to_tvdb_table()
+
+	
+	
+def imdb_table_to_tvdb_table():
+	conn = sqlite3.connect(sickadd_db)
+	c = conn.cursor()	
+	c.execute ("SELECT tvdb_id, imdb_name FROM imdb_fav WHERE tvdb_id is NOT NULL")
+	result_set = c.fetchall()
+	for imdb_fav_list in result_set:
+		tvdb_id =  int(imdb_fav_list[0])
+		imdb_name = ("IMDB - " + (imdb_fav_list[1]))
+		c.execute ("INSERT OR IGNORE INTO tvdb_fav VALUES (?,?,?)", (tvdb_id, imdb_name, 0))
+		conn.commit()
+
+
+
+
+
+
+
+
+
+##############################  SICKBEARD INTERACTIONS  ############################	
+
+## Is Sickbeard reachable?
 def sickbeard_check():
-
 	sb_http_status = urllib2.urlopen(sickbeard_api_url).getcode()
 	if sb_http_status != 200:
 		print "Sickbeard cannot be contacted. Make sure it's running and host and port are corrects"
@@ -127,8 +309,7 @@ def sickbeard_check():
 		print "Sickbeard is running. Continuing..."
 		sb_showlist_download()
 
-
-
+		
 
 
 ## Retrieve the list of all shows from Sickbeard
@@ -136,7 +317,8 @@ def sb_showlist_download():
 	#	Get the list from Sickbeard
 	print ("Retrieving Sickbeard show list")
 	sb_show_list_url =  sickbeard_api_url + "/?cmd=shows"
-	print "Getting show list from: " + sb_show_list_url
+	if debug == 1:
+		print "Getting show list from: " + sb_show_list_url
 	response = urllib2.urlopen(sb_show_list_url)
 	sb_show_list_response = json.load(response)
 	response.close()
@@ -150,6 +332,7 @@ def sb_showlist_download():
 		tvdb_show_status_update(sb_show_list_response['data'])
 
 
+		
 ## Changing TVDB records status to "in_SB" for 	any records found in SB
 def tvdb_show_status_update(sb_show_list):
 #	print "Parsing SickBeard show list into a list of TVDB ID"
@@ -160,18 +343,21 @@ def tvdb_show_status_update(sb_show_list):
 		sb_show_id = sb_show_list[item]
 		show_name = sb_show_id['show_name']
 		tvdbid = str(sb_show_id['tvdbid'])
-		print "Checking show: " + tvdbid + ": " + show_name
+		if debug == 1:
+			print "Checking show: " + tvdbid + ": " + show_name
 		c.execute ('UPDATE tvdb_fav SET state = 2 WHERE state != 2 AND tvdb_id  = ?', [tvdbid])
 		c.execute ('UPDATE tvdb_fav SET tvdb_name = ? WHERE tvdb_name != ? AND tvdb_id  = ?', (show_name, show_name, tvdbid))	
 	conn.commit()
 	conn.close()
 	print "Local database updated with current list of Sickbeard shows"
 
+	
 
 		
-
+## Add to SickBeard shows with TVDB IDs which are not already there
 def AddToSickbeard():
-	print "Adding to SickBeard favorites which are not already there"
+	if debug == 1:
+		print "Adding to SickBeard shows with TVDB IDs which are not already there"
 	sb_add_show_url =  sickbeard_api_url + "/?cmd=show.addnew&tvdbid="
 	conn = sqlite3.connect(sickadd_db)
 	c = conn.cursor()
@@ -179,8 +365,9 @@ def AddToSickbeard():
 	result_set = c.fetchall()
 	for tvdbid_list in result_set:
 		tvdb_id =  str(tvdbid_list[0])
-		print "Adding show with TVDB ID: " + tvdb_id
-		print sb_add_show_url + tvdb_id
+		if debug == 1:
+			print "Adding show with TVDB ID: " + tvdb_id
+			print sb_add_show_url + tvdb_id
 		response = urllib2.urlopen(sb_add_show_url + tvdb_id)
 		sb_add_show_response = json.load(response)
 		if not sb_add_show_response['result'] == "success":
@@ -193,18 +380,25 @@ def AddToSickbeard():
 
 
 
-	
-	
+################################### SCRIPT STARTUP ###########################################	
 
 def startup():
 	db_check()
+# Is TVDB Enabled?
 	if tvdb_enabled == 1:
-		print "TVDB Favorites are enabled"
-		tvdb_check()
+		print "TVDB Favorites are enabled. Processing TVDB"
+		tvdb_processing()
 	else:
-		print "TVDB Favorites are disabled. Skipping TVDB"
+		print "TVDB Favorites are disabled. Skipping TVDB Favorites download"
+
+# Is IMDB enabled?	
+	if imdb_enabled == 1:
+		print "IMDB watchlist is enabled. Downloading now IMDB watchlist"
+		imdb_processing(imdb_watchlist_rss_url)
+	else:
+		print "IMDB watchlist disabled. Skipping IMDB processing"
+	
 	sickbeard_check()
 	AddToSickbeard()	
 
 startup()
-
